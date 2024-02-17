7696dfcf01dd3bd97d295d5676a5609a254cfe88
Wassimlghaw <160129743+Wassimlghaw@users.noreply.github.com>
2024-02-17 20:40:37 +0100

    Update bingimg.js

--- plugins/bingimg.js
+++ plugins/bingimg.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     m.reply("تابع صاحب البوت في حساباته تشجيعا له ♥...\ninstagram.com/w_assim_2008")
 
     try {
-        let response = await fetch(`https://api.maelyn.my.id/api/bingimage?prompt=${text}&apikey=nkI6Qg3tfZ`)
+        let response = await fetch(`https://api.maelyn.my.id/api/bingimage?prompt=${text}&apikey=iIKqRj3q6A`)
         let result = await response.json()
 
         if (result.status === "Success" && result.result.length > 0) {
